0f4c59c85d3eb621057aff2e36a882af70eca2ef
Aurélie Digeon <aurelie.digeon@gmail.com>
2017-06-14 09:32:51 +0200

    Add Enum for Gender and Type semester

---
+++ PicSimulator/src/Enum/TypeSemestre.java
@@ -0,0 +1,8 @@
+package Enum;
+
+/**
+ * Created by aureliedigeon on 14/06/2017.
+ */
+public enum TypeSemestre {
+    TRONC_COMMUN, BRANCHE, DOCTORANT, ESCOM, HUTECH, DIPLOME, DOUBLE_DIPLOME, PROFESSEUR_CHERCHEUR,
+}
